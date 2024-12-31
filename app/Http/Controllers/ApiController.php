b5f4f285293edb19c30138bf60c312ecf325d3f7
Julfikar <julfikarasoft@gmail.com>
2024-12-31 12:44:31 +0600

    update
    updation on the api structures

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,96 +13,123 @@ class ApiController extends Controller
 {//category
     public function storeCategory(Request $request)
     {
-         Category::create($request->all());
-         return response()->json(['status'=>true, 'message'=>'Successfully category has been added']);
+        Category::create($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully category has been added'
+        ]);
     }
- 
-    public function updateCategory(Request $request,$id)
+
+    public function updateCategory(Request $request, $id)
     {
-         $category=Category::find($id);
-         $category->update($request->all());
-         return response()->json(['status'=>true, 'message'=>'Successfully category has been updated']);
+        $category = Category::find($id);
+        $category->update($request->all());
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully category has been updated'
+        ]);
     }
-    public function showCategory($id){
-         $category=Category::with('subcategories')->find($id);
-         return response()->json(['status'=>true, 'data'=>$category]);
+    public function showCategory($id)
+    {
+        $category = Category::with('subcategories')->find($id);
+        return response()->json([
+            'status' => true
+            ,
+            'data' => $category
+        ]);
 
     }
-    public function deleteCategory($id){
-        $category=Category::find($id);
-        $category->delete();         
-        return response()->json(['status'=>true, 'message'=>'Successfully category has been deleted']);
+    public function deleteCategory($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully category has been deleted'
+        ]);
 
     }
     //categories with sub categories
     public function categories()
     {
         $categories = Category::with('subcategories.products')->latest()->get();
-        return response()->json(['status'=>true, 'data'=>$categories]);
+        return response()->json([
+            'status' => true,
+            'data' => $categories
+        ]);
     }
 
     //subcategories
     public function storeSubcategory(Request $request)
     {
         Subcategory::create($request->all());
-        return response()->json(['status'=>true, 'message'=>'Successfully Subcategory has been added']);
+        return response()->json(['status' => true, 'message' => 'Successfully Subcategory has been added']);
     }
-    public function updateSubcategory(Request $request,$id)
+    public function updateSubcategory(Request $request, $id)
     {
-         $subCategory=Subcategory::find($id);
-         $subCategory->update($request->all());
-         return response()->json(['status'=>true, 'message'=>'Successfully Subcategory has been updated']);
+        $subCategory = Subcategory::find($id);
+        $subCategory->update($request->all());
+        return response()->json(['status' => true, 'message' => 'Successfully Subcategory has been updated']);
     }
-    public function showSubcategory($id){
-         $subCategory=Subcategory::find($id);
-         return response()->json(['status'=>true, 'data'=>$subCategory]);
+    public function showSubcategory($id)
+    {
+        $subCategory = Subcategory::find($id);
+        return response()->json(['status' => true, 'data' => $subCategory]);
 
     }
-    public function deleteSubcategory($id){
-        $subCategory=Subcategory::find($id)->delete();         
-        return response()->json(['status'=>true, 'message'=>'Successfully Subcategory has been deleted']);
+    public function deleteSubcategory($id)
+    {
+        $subCategory = Subcategory::find($id)->delete();
+        return response()->json(['status' => true, 'message' => 'Successfully Subcategory has been deleted']);
     }
-   
+
 
     //product
-    public function storeProduct(Request $request){
+    public function storeProduct(Request $request)
+    {
         Product::create($request->all());
-        return response()->json(['status'=>true,'message'=>'Successfully stored product']);
+        return response()->json(['status' => true, 'message' => 'Successfully stored product']);
     }
 
-    public function updateProduct(Request $request,$id){
-        $product=Product::find($id);
+    public function updateProduct(Request $request, $id)
+    {
+        $product = Product::find($id);
         $product->update($request->all());
-        return response()->json(['status'=>true,'message'=>'successfully product updated']);
+        return response()->json(['status' => true, 'message' => 'successfully product updated']);
 
     }
-    public function showProduct($id){
-        $productDetails=Product::find($id);
-        return response()->json(['status'=>true,'data'=>$productDetails]);
+    public function showProduct($id)
+    {
+        $productDetails = Product::find($id);
+        return response()->json(['status' => true, 'data' => $productDetails]);
     }
-     public function deleteProduct($id){
+    public function deleteProduct($id)
+    {
         Product::find($id)->delete();
-        return response()->json(['status'=>true,'message'=>'Product deleted successfully']);
+        return response()->json(['status' => true, 'message' => 'Product deleted successfully']);
     }
 
 
     //many to many relation
-    public function storeRole(Request $request){
+    public function storeRole(Request $request)
+    {
         Role::create($request->all());
-        return response()->json(['status'=>true,'message'=>'successfully added role']);
+        return response()->json(['status' => true, 'message' => 'successfully added role']);
     }
-     public function storeUser(Request $request){
+    public function storeUser(Request $request)
+    {
         User::create($request->all());
-        return response()->json(['status'=>true,'message'=>'successfully added User']);
+        return response()->json(['status' => true, 'message' => 'successfully added User']);
     }
-     public function assignRole(Request $request){
-         
-         $explodes = (explode(",",$request->role_ids));
-         
+    public function assignRole(Request $request)
+    {
+
+        $explodes = (explode(",", $request->role_ids));
+
         $user = User::find($request->user_id);
         $user->roles()->sync($explodes);
 
 
-        return response()->json(['status'=>true,'message'=>'successfully asigned role']);
+        return response()->json(['status' => true, 'message' => 'successfully asigned role']);
     }
 }
